96bd87fc12a1dfcc0edca2b575885319c637ce16
sahilcodexx <sahil171759@gmail.com>
2026-03-22 10:39:27 +0530

    Revert "Add custom styles"

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./index.css"
 
 function App() {
   return (
-    <div className="min-h-screen bg-neutral-950 flex items-center justify-center p-8">
+    <div className="min-h-screen bg-neutral-100 flex items-center justify-center p-8">
       <Keyboard enableHaptics={true} enableSound={true} />
     </div>
   )
