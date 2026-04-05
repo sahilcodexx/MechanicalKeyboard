30547cc2f7963790d0d0425072091fb838363e20
sahilcodexx <sahil171759@gmail.com>
2026-04-05 02:30:00 +0000

    Remove vercel analytics from the project

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import LandingPage from './LandingPage'
 import PreviewPage from './PreviewPage'
@@ -9,7 +8,6 @@ import PreviewPage from './PreviewPage'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <Analytics />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/preview" element={<PreviewPage />} />
